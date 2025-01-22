7b94cf4451758d5c984a8d54d18d98d037a4b903
Edward J <edjeong99@gmail.com>
2025-01-21 18:06:06 -0600

    add more response

--- src/util/botResponses.js
+++ src/util/botResponses.js
@@ -2,6 +2,7 @@
 const botResponses = {
     location_of_the_business: "We are located at 123 Main Street, Springfield.",
     business_hours: "We are open from 9 AM to 5 PM, Monday to Friday.",
+    edward_profile:"Edward is software developer with 6 years of experience in full-stack development, DB dev and dev-op.  He also had a long carrier in business that taught him many soft skills that helps to get jobs done"
     // Add more responses here...
   };
   
